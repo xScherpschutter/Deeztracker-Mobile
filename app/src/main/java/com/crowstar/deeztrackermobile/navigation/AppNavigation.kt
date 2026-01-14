08d56a96ee76d0bef724ea38106a14528e0cafe6
Willian <konradlisthrop@gmail.com>
2026-01-13 19:34:02 -0500

    add: ARL Storage

--- app/src/main/java/com/crowstar/deeztrackermobile/navigation/AppNavigation.kt
+++ app/src/main/java/com/crowstar/deeztrackermobile/navigation/AppNavigation.kt
@@ -1,17 +1,25 @@
 package com.crowstar.deeztrackermobile.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.crowstar.deeztrackermobile.features.rusteer.RustDeezerService
 import com.crowstar.deeztrackermobile.ui.screens.LoginScreen
 import com.crowstar.deeztrackermobile.ui.screens.SearchScreen
 
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
+    val context = LocalContext.current
     
-    NavHost(navController = navController, startDestination = "login") {
+    // Check if user is already logged in
+    val rustService = remember { RustDeezerService(context) }
+    val startDestination = if (rustService.isLoggedIn()) "search" else "login"
+    
+    NavHost(navController = navController, startDestination = startDestination) {
         composable("login") {
             LoginScreen(onLoginSuccess = {
                 navController.navigate("search") {
