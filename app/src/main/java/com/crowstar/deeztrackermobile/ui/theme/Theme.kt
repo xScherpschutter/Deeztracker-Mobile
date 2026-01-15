fc6e36b927e6355928605deba6581f5393a675e9
Willian <konradlisthrop@gmail.com>
2026-01-14 22:05:48 -0500

    edit: Status bar color

--- app/src/main/java/com/crowstar/deeztrackermobile/ui/theme/Theme.kt
+++ app/src/main/java/com/crowstar/deeztrackermobile/ui/theme/Theme.kt
@@ -53,8 +53,8 @@ fun DeeztrackerMobileTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = BackgroundDark.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
     }
 
