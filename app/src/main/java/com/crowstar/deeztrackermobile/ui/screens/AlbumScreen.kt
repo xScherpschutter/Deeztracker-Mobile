e5abd41961e56489190fb1eb03d4a6678b9260c2
Willian <konradlisthrop@gmail.com>
2026-01-21 17:52:52 -0500

    fix: Album/Playlist View UI Refresh

--- app/src/main/java/com/crowstar/deeztrackermobile/ui/screens/AlbumScreen.kt
+++ app/src/main/java/com/crowstar/deeztrackermobile/ui/screens/AlbumScreen.kt
@@ -47,8 +47,8 @@ fun AlbumScreen(
     val context = LocalContext.current
     val downloadManager = remember { DownloadManager.getInstance(context) }
     val downloadState by downloadManager.downloadState.collectAsState()
+    val downloadRefreshTrigger by downloadManager.downloadRefreshTrigger.collectAsState()
     val snackbarHostState = remember { SnackbarHostState() }
-    val scope = rememberCoroutineScope()
 
     LaunchedEffect(albumId) {
         viewModel.loadAlbum(albumId)
@@ -92,7 +92,6 @@ fun AlbumScreen(
                     }
                 },
                 actions = {
-                    val isDownloading = downloadState is DownloadState.Downloading
                 },
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = Color.Transparent
@@ -170,8 +169,8 @@ fun AlbumScreen(
                     val track = tracks[index]
                     var isDownloaded by remember { mutableStateOf(false) }
                     
-                    // Check if track is downloaded
-                    LaunchedEffect(track.id) {
+                    // Check if track is downloaded, re-check when refresh trigger changes
+                    LaunchedEffect(track.id, downloadRefreshTrigger) {
                         isDownloaded = downloadManager.isTrackDownloaded(
                             track.title,
                             track.artist?.name ?: ""
