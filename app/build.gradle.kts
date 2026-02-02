619059fa17daa0fcb06c59f7f7acfc0a679de0d4
Willian <konradlisthrop@gmail.com>
2026-02-02 01:25:38 -0500

    edit: app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.crowstar.deeztrackermobile"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
